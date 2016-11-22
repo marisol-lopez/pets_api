9dd1f1feeee902a460abab70c296c601754d4198
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-11-21 21:17:51 -0800

    pet create functionality

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,6 +13,22 @@ class PetsController < ApplicationController
       :status => :ok
   end
 
+
+  def create
+    pet = Pet.new(
+      name: params[:name],
+      breed: params[:breed],
+      age: params[:age]
+    )
+
+    if pet.save
+      render :json => pet.to_json, :callback => params['callback'],
+      :status => :ok
+    else
+      render :json => [], :callback => params['callback'], :status => :no_content
+    end
+  end
+
   def dibs
     pet = Pet.find(params[:id])
     unless pet.owner
